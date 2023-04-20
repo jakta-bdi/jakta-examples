7bb40a8e4e1534957af1c5be1f6356e8591b7ddf
Martina Baiardi <martina.baiardi4@studio.unibo.it>
2023-04-20 16:31:10 +0200

    chore: add jakta examples

---
+++ src/main/kotlin/it/unibo/jakta/agents/examples/telephonegame/TelephoneProgramLiterals.kt
@@ -0,0 +1,12 @@
+package it.unibo.jakta.agents.examples.telephonegame
+
+import it.unibo.jakta.agents.examples.OwnName
+object TelephoneProgramLiterals {
+    val receiver: String by OwnName
+    val source: String by OwnName
+    val start: String by OwnName
+    val self: String by OwnName
+    val print: String by OwnName
+    val send: String by OwnName
+    val count: String by OwnName
+}
