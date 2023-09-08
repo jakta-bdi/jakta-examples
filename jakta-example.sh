aeae46d233c8191d3a957a70b68e531c86551be5
Danilo Pianini <danilo.pianini@gmail.com>
2023-09-08 12:51:50 +0200

    fix(autolaunch): fix shell expansion

--- jakta-example.sh
+++ jakta-example.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
-git clone https://github.com/jakta-bdi/jakta-examples.git "~/Downloads/jakta-examples-${date --utc "+%F-%H.%M.%S"}"
-cd ~/Downloads/jakta-examples
+DESTINATION=~"/Downloads/jakta-examples-$(date --utc "+%F-%H.%M.%S")"
+git clone https://github.com/jakta-bdi/jakta-examples.git "$DESTINATION"
+cd "$DESTINATION"
 ./gradlew jaktaPingPong
