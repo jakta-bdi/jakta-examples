b5a6b2590d22db8cfea4404965716619a3df29b8
Martina Baiardi <martina.baiardi4@studio.unibo.it>
2023-09-08 16:49:25 +0200

    fix: fix location for download in jakta quick-start script

--- jakta-example.sh
+++ jakta-example.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-DESTINATION=~"/Downloads/jakta-examples-$(date --utc "+%F-%H.%M.%S")"
+DESTINATION="$HOME/Downloads/jakta-examples-$(date --utc "+%F-%H.%M.%S")"
 git clone https://github.com/jakta-bdi/jakta-examples.git "$DESTINATION"
 cd "$DESTINATION"
 ./gradlew jaktaPingPong
